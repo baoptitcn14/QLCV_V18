0c66db5a0b9590d4767c1b2757f9bba7eb7021ac
Gia Hân <nguyenhan.110401@gmail.com>
2025-03-10 10:09:01 +0700

    update

---
+++ src/app/setting/report-data/report-data.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-report-data',
+  standalone: true,
+  imports: [],
+  templateUrl: './report-data.component.html',
+  styleUrl: './report-data.component.scss'
+})
+export class ReportDataComponent {
+
+}
---
+++ src/app/setting/template-report/template-report.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-report',
+  standalone: true,
+  imports: [],
+  templateUrl: './template-report.component.html',
+  styleUrl: './template-report.component.scss'
+})
+export class TemplateReportComponent {
+
+}
