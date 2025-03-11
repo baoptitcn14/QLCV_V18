b826f47d0d43914c3508427e9e596e7e0deeeb1a
ChauGiangsay2 <124231363+ChauGiangsay2@users.noreply.github.com>
2025-03-11 08:46:51 +0700

    abc

---
+++ src/app/setting/report/define-date-time/define-date-time.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-define-date-time',
+  standalone: true,
+  imports: [],
+  templateUrl: './define-date-time.component.html',
+  styleUrl: './define-date-time.component.scss'
+})
+export class DefineDateTimeComponent {
+
+}
