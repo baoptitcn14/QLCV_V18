2ad70e09b95605df7045b0703a626c926bcbc6b8
ChauGiangsay2 <124231363+ChauGiangsay2@users.noreply.github.com>
2025-04-16 09:00:22 +0700

    check in

--- src/app/search/notify-search/notify-search.component.ts
+++ src/app/search/notify-search/notify-search.component.ts
@@ -1,12 +1,23 @@
+import { TableModule } from 'primeng/table';
 import { Component } from '@angular/core';
+import { ToolbarModule } from 'primeng/toolbar';
+import { DropdownModule } from 'primeng/dropdown';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-notify-search',
   standalone: true,
-  imports: [],
+  imports: [
+    ToolbarModule,
+    DropdownModule,
+    ButtonModule,
+  ],
   templateUrl: './notify-search.component.html',
   styleUrl: './notify-search.component.scss'
 })
 export class NotifySearchComponent {
-
+  onRefresh =  { emit: () => console.log('Làm mới') };
+  onReport  =  { emit: () => console.log('Xuất Excel') };
+    
 }
+
